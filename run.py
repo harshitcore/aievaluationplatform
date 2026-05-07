89b636f7b75b4c61442d1440c7027a9476e2f1ce
harshitgithub7 <hsharmaharshitcr7@gmail.com>
2026-05-08 01:00:15 +0530

    Auto-initialize database to bypass Render Shell

--- run.py
+++ run.py
@@ -4,12 +4,15 @@ from threading import Timer
 
 app = create_app()
 
+# Automatically create database tables if they don't exist
+# This ensures Render initializes the PostgreSQL database automatically!
+with app.app_context():
+    db.create_all()
+
 def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     # Wait 1.5 seconds for the server to spin up, then launch the browser
     Timer(1.5, open_browser).start()
     # Debug=False is important for executables so it doesn't double-load
